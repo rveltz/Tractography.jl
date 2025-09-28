9e78b895cb0731017f4cef0a34be1616c5e6a906
romain veltz <romain.veltz@inria.fr>
2025-09-28 11:15:53 +0200

    add main module

--- src/Tractography.jl
+++ src/Tractography.jl
@@ -1,5 +1,27 @@
 module Tractography
+    using DocStringExtensions
+    using Accessors
+    using NIfTI
+    using Parameters, StatsBase, LinearAlgebra
+    import StaticArrays as SA
+    using LoopVectorization
+    import FastTransforms
 
-# Write your package code here.
+    # sampling method of FODF
+    export FibonacciSH, ComputeAllODF
+    export ODFData, TMC, Cone, sample_odf, sample, init
+    export CSD, Rejection, Deterministic, Connectivity
+    export save_streamlines
 
+    # plotting
+    export plot_streamlines!, plot_odf, plot_odf!
+
+    include("plot.jl")
+    include("tmc.jl")
+    include("utils.jl")
+    include("data.jl")
+    include("sph.jl")
+    include("tmccache.jl")
+    include("sample.jl")
+    include("gpu.jl")
 end
