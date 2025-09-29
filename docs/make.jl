41d933f5b633a077e032ae2d741ab999c46fc791
romain veltz <romain.veltz@inria.fr>
2025-09-29 20:28:10 +0200

    refactor make.jl

--- docs/make.jl
+++ docs/make.jl
@@ -6,38 +6,37 @@ using Documenter, CairoMakie, Tractography
 ENV["JULIA_DEBUG"] = Documenter
 
 makedocs(
-	modules = [Tractography, isdefined(Base, :get_extension) ?
+    modules = [Tractography, isdefined(Base, :get_extension) ?
     Base.get_extension(Tractography, :MakieExt) :
     Tractography.MakieExt],
-	doctest = false,
-	warnonly = true,
-	sitename = "Tractography in Julia",
-	format = Documenter.HTML(
-		edit_link = "master", 
-		collapselevel = 1
-	),
-	# format = Documenter.LaTeX(),
-	warnonly = true,
-	draft = false,
-	pagesonly = true, # do not compile what is not in pages =
-	authors = "Samuel Deslauriers-Gauthier and Romain Veltz",
-	pages = Any[
-		"Home" => "index.md",
-		"Getting started" => "getstart.md",
-		"Tutorials" => [
-				# "basic.md",
-				"fcup.md",
-				"gpu.md",
-		],
-		"Algorithms" => "algos.md",
-		"Plotting" => "plot.md",
-		"Library" => "library.md"
-	],
-	repo = Documenter.Remotes.GitHub("rveltz", "Tractography.jl"),
-	)
+    doctest = false,
+    sitename = "Tractography in Julia",
+    format = Documenter.HTML(
+        edit_link = "master", 
+        collapselevel = 1
+    ),
+    # format = Documenter.LaTeX(),
+    warnonly = true,
+    draft = false,
+    pagesonly = true, # do not compile what is not in pages =
+    authors = "Samuel Deslauriers-Gauthier and Romain Veltz",
+    pages = Any[
+        "Home" => "index.md",
+        "Getting started" => "getstart.md",
+        "Tutorials" => [
+                # "basic.md",
+                "fcup.md",
+                "gpu.md",
+        ],
+        "Algorithms" => "algos.md",
+        "Plotting" => "plot.md",
+        "Library" => "library.md"
+    ],
+    repo = Documenter.Remotes.GitHub("rveltz", "Tractography.jl"),
+    )
 
 deploydocs(;
-	repo = "github.com/rveltz/Tractography.jl.git",
-	push_preview = true, 
-	target = "build", 
-	devbranch = "main")
+    repo = "github.com/rveltz/Tractography.jl.git",
+    push_preview = true, 
+    target = "build", 
+    devbranch = "main")
